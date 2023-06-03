d8697504590d1004e36c44a39345665be971781e
seikou <seikou615@gmail.com>
2023-06-03 11:48:34 +0100

    Added: `List` component

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,15 @@
+import { Form, List } from "components"
+
+import React from 'react'
+import { StyledMain } from './Main.styles'
+
+function Main() {
+  return (
+    <StyledMain>
+      
+      <List />
+    </StyledMain>
+  )
+}
+
+export default Main
