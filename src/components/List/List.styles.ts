d65af8cdcd13217f6aec47ebfa7edcad7ff1a67c
seikou <seikou615@gmail.com>
2023-06-03 11:49:01 +0100

    Added: `List` components styles

---
+++ src/components/List/List.styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const StyledList = styled.ul`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`
+
+export const ItemContainer = styled.div`
+ display: flex;
+ align-items: center;
+ gap: 13px;
+`
+
+export const ListItem = styled.li`
+    color: #222741;
+`
