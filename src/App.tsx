3948e40893e0074e6c8a81d924fd10445e928560
seikou <seikou615@gmail.com>
2023-06-27 10:53:22 +0100

    Wrapped `App` component and used appcontext to conditionally render

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import { Main, Modal } from "components";
+import React, { Fragment } from "react";
+
+import GlobalStyles from "global/GlobalStyle";
+import { useAppContext } from "hooks/useAppContext";
 
 function App() {
+
+  const { modalOpen } = useAppContext()
+
   return (
-    null
-  )
+    <Fragment>
+      <GlobalStyles />
+      {modalOpen ? <Modal /> : <Main />}
+    </Fragment>
+  );
 }
 
 export default App;
