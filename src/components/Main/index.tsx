941bad7a6484f5ad46c22912b6173eeb6a3ec893
seikou <seikou615@gmail.com>
2023-06-27 10:56:05 +0100

    Added: more components and data

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,28 @@
-import { Form, List } from "components"
+import {
+  Body,
+  Container,
+  MobileImage,
+  StyledImage,
+  StyledMain,
+  Title,
+} from "./Main.styles";
+import { Form, List } from "components";
 
-import React from 'react'
-import { StyledMain } from './Main.styles'
+import React from "react";
 
 function Main() {
   return (
     <StyledMain>
-      
-      <List />
+      <Container>
+        <Title>Stay Updated!</Title>
+        <Body>Join 60,000+ product managers receiving monthly updates on:</Body>
+        <List />
+        <Form />
+      </Container>
+      <StyledImage src={"images/illustration-sign-up-desktop.svg"} />
+      <MobileImage src={"images/illustration-sign-up-mobile.svg"} />
     </StyledMain>
-  )
+  );
 }
 
-export default Main
+export default Main;
